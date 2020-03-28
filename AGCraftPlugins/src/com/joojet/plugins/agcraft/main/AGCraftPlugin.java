8b44a856a602bd0e649b39ad3959f65f6eb9b2f6
Eric Su <ericsu14@hotmail.com>
2020-03-27 18:53:06 -0700

    Added a new getcoordinates command!

--- AGCraftPlugins/src/com/joojet/plugins/agcraft/main/AGCraftPlugin.java
+++ AGCraftPlugins/src/com/joojet/plugins/agcraft/main/AGCraftPlugin.java
@@ -5,6 +5,7 @@ import com.joojet.biblefetcher.database.CreateDatabase;
 import com.joojet.biblefetcher.interpreter.CommandInterpreter;
 import com.joojet.plugins.biblefetcher.commands.Bible;
 import com.joojet.plugins.biblefetcher.commands.ClearBibles;
+import com.joojet.plugins.coordinates.commands.GetCoordinates;
 public class AGCraftPlugin extends JavaPlugin 
 {
 	public static CommandInterpreter interpreter = new CommandInterpreter ();
@@ -17,6 +18,7 @@ public class AGCraftPlugin extends JavaPlugin
 		
 		this.getCommand("bible").setExecutor(new Bible ());
 		this.getCommand("clearBibles").setExecutor(new ClearBibles());
+		this.getCommand("getCoordinates").setExecutor(new GetCoordinates());
 	}
 	
 	@Override
---
+++ AGCraftPlugins/src/com/joojet/plugins/coordinates/commands/GetCoordinates.java
@@ -0,0 +1,64 @@
+package com.joojet.plugins.coordinates.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class GetCoordinates implements CommandExecutor 
+{
+	/** A simple command that tells the player their current coordinates
+	 * 	Usage:
+	 * 		/coordinates - Returns your current position
+	 * 		/coordinates <player> Returns the coordinates of another player in the server */
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String [] args) 
+	{
+		int n = args.length;
+		
+		if (sender instanceof Player)
+		{
+			Player player = (Player) sender;
+			if (n == 0)
+			{
+				player.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + GetCoordinates.getCoordinates(player));
+			}
+			if (n >= 1)
+			{
+				String otherPlayer = args[0];
+				Player p2 = Bukkit.getServer().getPlayer(otherPlayer);
+				
+				if (p2 != null)
+				{
+					player.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + otherPlayer + "'s " + getCoordinates (p2));
+				}
+				else
+				{
+					player.sendMessage(ChatColor.RED + "" + ChatColor.ITALIC + "Either " + otherPlayer + " is not logged in or " + otherPlayer + " is not a valid user in this server.");
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	/** Returns the player's current coordinates as a formatted String */
+	public static String getCoordinates (Player player)
+	{
+		Location loc = player.getLocation();
+		StringBuilder coordinates = new StringBuilder ();
+		coordinates.append ("Coordinates: { X=");
+		coordinates.append (String.format("%.3f", loc.getX()));
+		coordinates.append (", Y=");
+		coordinates.append (String.format("%.3f", loc.getY()));
+		coordinates.append (", Z=");
+		coordinates.append (String.format("%.3f", loc.getZ()));
+		coordinates.append (" }");
+		
+		return coordinates.toString();
+	}
+}
