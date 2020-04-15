a4476204f537c29642ff7f50e54f7fd53febdffe
Eric Su <ericsu14@hotmail.com>
2020-04-15 11:35:16 -0700

    Removed needless interfaces

--- BibleFetcher/src/com/joojet/biblefetcher/main/PercussionHandler.java
+++
@@ -1,7 +0,0 @@
-package com.joojet.biblefetcher.main;
-
-import be.tarsos.dsp.onsets.OnsetHandler;
-
-public interface PercussionHandler extends OnsetHandler {
-
-}
--- BibleFetcher/src/com/joojet/biblefetcher/main/TestAudioPercussionSound.java
+++ BibleFetcher/src/com/joojet/biblefetcher/main/TestAudioPercussionSound.java
@@ -9,6 +9,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.io.jvm.AudioDispatcherFactory;
+import be.tarsos.dsp.onsets.OnsetHandler;
 import be.tarsos.dsp.onsets.PercussionOnsetDetector;
 
 public class TestAudioPercussionSound 
@@ -20,14 +21,14 @@ public class TestAudioPercussionSound
 		AudioFormat format = AudioSystem.getAudioInputStream(new File (file)).getFormat();
 		AudioDispatcher dispatcher = AudioDispatcherFactory.fromFile(new File (file), 2048, 0);
 		
-		dispatcher.addAudioProcessor(new PercussionOnsetDetector (format.getSampleRate(), 2048, new PercussionHandler () {
+		dispatcher.addAudioProcessor(new PercussionOnsetDetector (format.getSampleRate(), 2048, new OnsetHandler () {
 			int i = 0;
 			@Override
 			public void handleOnset (double timestamp, double o)
 			{
 				System.out.println (i++ + "\t" + timestamp + "\t" + o);
 			}
-		}, 50, 4));
+		}, 44, 4));
 		System.out.println (dispatcher.toString());
 		dispatcher.run();
 	}
