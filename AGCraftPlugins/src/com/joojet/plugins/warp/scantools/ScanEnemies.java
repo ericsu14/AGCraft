ea48ce1671ab688ea0c7f8ae9cc3abf47c05ee2a
Eric Su <ericsu14@hotmail.com>
2020-07-01 14:41:13 -0700

    Reduced z scan range for the checkEnemies command while using warp

--- AGCraftPlugins/src/com/joojet/plugins/warp/scantools/ScanEnemies.java
+++ AGCraftPlugins/src/com/joojet/plugins/warp/scantools/ScanEnemies.java
@@ -11,8 +11,8 @@ public class ScanEnemies
 	/** Returns true if there are enemies within an r-block radius of the current player */
 	public static boolean ScanNearbyEnemies (Player p, int radius)
 	{
-		int halfRadius = radius;
-		ArrayList <Entity> entites = (ArrayList<Entity>) p.getNearbyEntities(halfRadius, halfRadius, halfRadius);
+		int halfRadius = (int) (radius / 2.0);
+		ArrayList <Entity> entites = (ArrayList<Entity>) p.getNearbyEntities(radius, radius, halfRadius);
 		
 		for (Entity e : entites)
 		{
