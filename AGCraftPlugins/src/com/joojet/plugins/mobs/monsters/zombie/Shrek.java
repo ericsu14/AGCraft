a2d72cbdc1b66427c34e2b42d0a557292116f4f0
Eric Su <ericsu14@hotmail.com>
2020-07-13 01:19:47 -0700

    Buffed some mobs

--- AGCraftPlugins/src/com/joojet/plugins/mobs/monsters/zombie/Shrek.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/monsters/zombie/Shrek.java
@@ -3,6 +3,7 @@ package com.joojet.plugins.mobs.monsters.zombie;
 import org.bukkit.ChatColor;
 import org.bukkit.block.Biome;
 
+import com.joojet.plugins.mobs.enums.CustomPotionEffect;
 import com.joojet.plugins.mobs.equipment.boots.OgreBoots;
 import com.joojet.plugins.mobs.equipment.chest.OgreTunic;
 import com.joojet.plugins.mobs.equipment.head.ShrekHat;
@@ -17,7 +18,7 @@ public class Shrek extends MobEquipment
 		this.color = ChatColor.DARK_GREEN;
 		this.name = "Shrek";
 		this.addBiomes(Biome.SWAMP, Biome.SWAMP_HILLS);
-		
+		this.addPotionEffect(CustomPotionEffect.SPEED);
 		this.helmet = new ShrekHat (this.color);
 		this.chestplate = new OgreTunic (this.color);
 		this.leggings = new OgreLeggings (this.color);
