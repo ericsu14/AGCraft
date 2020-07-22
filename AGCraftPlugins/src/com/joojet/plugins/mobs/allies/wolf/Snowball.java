64af920d88d276e7565fed39aca5aad9394d46c4
Eric Su <ericsu14@hotmail.com>
2020-07-22 16:58:54 -0700

    Gave snowball his armor

--- AGCraftPlugins/src/com/joojet/plugins/mobs/allies/wolf/Snowball.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/allies/wolf/Snowball.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.block.Biome;
 
 import com.joojet.plugins.mobs.enums.CustomPotionEffect;
+import com.joojet.plugins.mobs.equipment.chest.SnowballHeart;
 import com.joojet.plugins.mobs.interfaces.MobEquipment;
 
 public class Snowball extends MobEquipment
@@ -14,6 +15,7 @@ public class Snowball extends MobEquipment
 		this.color = ChatColor.AQUA;
 		this.name = "Snowball";
 		this.addPotionEffect(CustomPotionEffect.RESISTANCE);
-		
+		this.chestplate = new SnowballHeart (this.color);
+		this.setDropRates(0.0f, 1.15f, 0.0f, 0.0f, 0.0f, 0.0f);
 	}
 }
