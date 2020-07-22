d200de62526ed135f0131a083c5f5e84683faa75
Eric Su <ericsu14@hotmail.com>
2020-07-22 15:36:12 -0700

    Renamed Alpha Wolf's head to Cookie's Heart and made it droppable to make the player feel bad

--- AGCraftPlugins/src/com/joojet/plugins/mobs/allies/wolf/Cookie.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/allies/wolf/Cookie.java
@@ -4,7 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.block.Biome;
 
 import com.joojet.plugins.mobs.enums.CustomPotionEffect;
-import com.joojet.plugins.mobs.equipment.head.AlphaWolfHead;
+import com.joojet.plugins.mobs.equipment.chest.CookieHeart;
 import com.joojet.plugins.mobs.interfaces.MobEquipment;
 
 public class Cookie extends MobEquipment
@@ -12,9 +12,11 @@ public class Cookie extends MobEquipment
 	public Cookie ()
 	{
 		this.addBiomes(Biome.THE_VOID);
+		this.setDropRates(0.00f, 1.15f, 0.00f, 0.00f, 0.00f, 0.00f);
 		this.color = ChatColor.GOLD;
 		this.name = "Cookie";
 		this.addPotionEffect(CustomPotionEffect.REGEN);
-		this.helmet = new AlphaWolfHead (this.color);
+		this.addPotionEffect(CustomPotionEffect.FIRE_RESISTANCE);
+		this.chestplate = new CookieHeart (this.color);
 	}
 }
---
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/equipment/chest/CookieHeart.java
@@ -0,0 +1,27 @@
+package com.joojet.plugins.mobs.equipment.chest;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.EquipmentSlot;
+
+import com.joojet.plugins.mobs.interfaces.Equipment;
+
+public class CookieHeart extends Equipment
+{
+	public CookieHeart (ChatColor color)
+	{
+		super (Material.COOKIE, EquipmentSlot.CHEST, color);
+		this.addDefenseAttributes(14.0, 8.0, 0.5);
+		this.addAttackAttributes(6.0, 0.0);
+		this.addSpeedAttribute(0.25);
+		this.addHealthAttributes(10.0);
+		this.setDisplayName("Cookie's Heart");
+		this.addLoreToItemMeta("Contains the heart of a beautiful soul.");
+		this.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 3);
+		this.addUnsafeEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 5);
+		this.addUnsafeEnchantment(Enchantment.PROTECTION_FALL, 4);
+		this.addUnsafeEnchantment(Enchantment.THORNS, 3);
+		this.addUnsafeEnchantment(Enchantment.DEPTH_STRIDER, 5);
+	}
+}
