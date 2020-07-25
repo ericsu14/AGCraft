9359d9fab2b4484c089ef100e75f83918ef83742
Eric Su <ericsu14@hotmail.com>
2020-07-24 17:01:57 -0700

    Modified description of clown head again

--- AGCraftPlugins/src/com/joojet/plugins/mobs/equipment/head/ClownHead.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/equipment/head/ClownHead.java
@@ -20,8 +20,8 @@ public class ClownHead extends Equipment
 		StringBuilder lore = new StringBuilder ();
 		lore.append("- - - - ");
 		lore.append("BAD SPORT! Because of your recent actions in this server,");
-		lore.append(" you will be forced to wear this clown head as consequence for ");
-		lore.append("your actions. This head will disappear from your inventory on: ");
+		lore.append(" you will be forced to wear this clown head as consequence. ");
+		lore.append("This head will disappear from your inventory on: ");
 		lore.append(ChatColor.AQUA);
 		lore.append(expirationTimestamp.get(Calendar.MONTH) + 1);
 		lore.append("/");
