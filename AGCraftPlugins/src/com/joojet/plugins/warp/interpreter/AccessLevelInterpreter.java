bfa423db5dadf3f25b62ec8d43ee40c215db2958
Eric Su <ericsu14@hotmail.com>
2020-07-30 16:12:40 -0700

    Removed redundant code from AccessLevelInterpreter

--- AGCraftPlugins/src/com/joojet/plugins/warp/interpreter/AccessLevelInterpreter.java
+++ AGCraftPlugins/src/com/joojet/plugins/warp/interpreter/AccessLevelInterpreter.java
@@ -1,30 +1,13 @@
 package com.joojet.plugins.warp.interpreter;
 
-import com.joojet.biblefetcher.trie.TrieNode;
-import com.joojet.biblefetcher.trie.TrieUtil;
+import com.joojet.plugins.agcraft.interfaces.AbstractInterpreter;
 import com.joojet.plugins.warp.constants.*;
 
-public class AccessLevelInterpreter 
+public class AccessLevelInterpreter extends AbstractInterpreter <WarpAccessLevel>
 {
-	private TrieNode <WarpAccessLevel> accessRoot;
-	
 	public AccessLevelInterpreter ()
 	{
-		accessRoot = new TrieNode <WarpAccessLevel> (' ', null);
-		this.populateTrie();
-	}
-	
-	private void populateTrie ()
-	{
-		for (WarpAccessLevel level : WarpAccessLevel.values())
-		{
-			TrieUtil.insertWord(level.name(), level, accessRoot);
-		}
-	}
-	
-	public WarpAccessLevel searchTrie (String input)
-	{
-		return TrieUtil.searchTrie(input, accessRoot);
+		super (WarpAccessLevel.values());
 	}
 }
 
