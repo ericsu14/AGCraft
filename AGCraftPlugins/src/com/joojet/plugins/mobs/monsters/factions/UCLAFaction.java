3706f137af785eb816c2e6a878947f50dc019339
Eric Su <ericsu14@hotmail.com>
2020-08-26 17:29:04 -0700

    The UCLA faction now hunts spiders

--- AGCraftPlugins/src/com/joojet/plugins/mobs/monsters/factions/UCLAFaction.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/monsters/factions/UCLAFaction.java
@@ -14,7 +14,8 @@ public class UCLAFaction extends MobEquipment
 		this.addFactions(Faction.UCLA);
 		this.addRivalFactions(Faction.USC, Faction.PHANTOM, Faction.DOOM_GUY);
 		this.addTargetsToHitList(EntityType.ZOMBIE, EntityType.SKELETON,
-				EntityType.PLAYER, EntityType.PHANTOM, EntityType.WITHER_SKELETON);
+				EntityType.PLAYER, EntityType.PHANTOM, EntityType.WITHER_SKELETON, 
+				EntityType.SPIDER);
 		this.addEntitiesToIgnoreList(EntityType.CREEPER);
 	}
 }
