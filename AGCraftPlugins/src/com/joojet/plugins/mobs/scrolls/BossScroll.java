2f0e9fad9c4952ba1eb72b2d1e5042b2494053e5
Eric Su <ericsu14@hotmail.com>
2020-08-28 18:33:28 -0700

    Increased words per line for summoning scroll

--- AGCraftPlugins/src/com/joojet/plugins/mobs/scrolls/BossScroll.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/scrolls/BossScroll.java
@@ -10,6 +10,7 @@ public class BossScroll extends SummoningScroll
 	public BossScroll (MobEquipment equipment, EntityType type)
 	{
 		super (equipment, type);
+		this.wordsPerLine = 9;
 		this.loreColor = ChatColor.DARK_RED;
 		this.addLoreToItemMeta("WARNING! A powerful mob lurks inside of this scroll. Use with extreme caution!");
 	}
