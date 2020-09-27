d249978493c507e7e234ccd1ad34513808e438be
Eric Su <ericsu14@hotmail.com>
2020-09-27 11:02:27 -0700

    USC mobs now target Vexes and Piglin Brutes

--- AGCraftPlugins/src/com/joojet/plugins/mobs/monsters/factions/USCFaction.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/monsters/factions/USCFaction.java
@@ -26,7 +26,8 @@ public abstract class USCFaction extends MobEquipment
 				EntityType.RAVAGER, EntityType.WITHER_SKELETON, EntityType.WITCH,
 				EntityType.GHAST, EntityType.GUARDIAN, EntityType.ELDER_GUARDIAN,
 				EntityType.SLIME, EntityType.MAGMA_CUBE, EntityType.ENDERMITE, EntityType.PHANTOM,
-				EntityType.ZOMBIFIED_PIGLIN, EntityType.WITHER, EntityType.GIANT, EntityType.HOGLIN);
+				EntityType.ZOMBIFIED_PIGLIN, EntityType.WITHER, EntityType.GIANT, EntityType.HOGLIN,
+				EntityType.VEX, EntityType.PIGLIN_BRUTE);
 		
 		this.addEntitiesToIgnoreList(EntityType.PLAYER, EntityType.WOLF, EntityType.CAT,
 				EntityType.IRON_GOLEM, EntityType.SNOWMAN, EntityType.DOLPHIN, EntityType.VILLAGER,
