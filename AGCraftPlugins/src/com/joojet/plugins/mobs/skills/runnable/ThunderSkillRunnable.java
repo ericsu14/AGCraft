96ce74289b3a2d88164f185e865781933fd9e485
Eric Su <ericsu14@hotmail.com>
2020-12-31 10:01:38 -0800

    NoDamageTicks are applied to surrounding allies instead of resistance for lightning skill

--- AGCraftPlugins/src/com/joojet/plugins/mobs/skills/runnable/ThunderSkillRunnable.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/skills/runnable/ThunderSkillRunnable.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import com.joojet.plugins.mobs.skills.attack.ThundagaSkill;
@@ -49,13 +47,12 @@ public class ThunderSkillRunnable extends BukkitRunnable {
 		// Gives caster (and all surrounding allies) invincibility for 3 ticks so they can survive his own thunder 
 		if (this.ticks == 1)
 		{
-			PotionEffect resistance = new PotionEffect (PotionEffectType.DAMAGE_RESISTANCE, 10, 5);
-			this.caster.addPotionEffect(resistance);
+			this.caster.setNoDamageTicks(this.caster.getMaximumNoDamageTicks());
 			for (LivingEntity ally : this.allies)
 			{
 				if (ally != null && !ally.isDead())
 				{
-					ally.addPotionEffect(resistance);
+					ally.setNoDamageTicks(ally.getMaximumNoDamageTicks());
 				}
 			}
 						
