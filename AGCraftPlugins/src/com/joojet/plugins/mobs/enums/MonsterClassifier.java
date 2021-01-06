c907c0b9af0c7e616f32c548b0e29d0266605d4a
Eric Su <ericsu14@hotmail.com>
2021-01-05 22:44:02 -0800

    Monster classifier table updated to make the game more fair
    Game is more balanced towards iron armor users. Player gear will need to have much stronger enchants in order for the more powerful custom mobs to spawn.

--- AGCraftPlugins/src/com/joojet/plugins/mobs/enums/MonsterClassifier.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/enums/MonsterClassifier.java
@@ -3,10 +3,10 @@ package com.joojet.plugins.mobs.enums;
 public enum MonsterClassifier 
 {
 	COMMON (0.0),
-	UNCOMMON (0.15),
-	RARE (0.30),
-	EPIC (0.45),
-	LEGENDARY (0.60),
+	UNCOMMON (0.35),
+	RARE (0.45),
+	EPIC (0.60),
+	LEGENDARY (0.70),
 	MYTHIC (0.80);
 	
 	/** Required threshold needed to be reached in order for the monster under this classifier is able to naturally
