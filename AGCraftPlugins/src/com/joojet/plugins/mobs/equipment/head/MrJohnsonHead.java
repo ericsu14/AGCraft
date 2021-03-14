8d1a555b99938c5ca17b04ca2cf0baba6a7ca188
Eric Su <ericsu14@hotmail.com>
2021-03-13 16:51:42 -0800

    Update MrJohnsonHead.java

--- AGCraftPlugins/src/com/joojet/plugins/mobs/equipment/head/MrJohnsonHead.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/equipment/head/MrJohnsonHead.java
@@ -14,7 +14,7 @@ public class MrJohnsonHead extends Equipment
 	{
 		super(EquipmentType.MR_JOHNSON_HEAD, PlayerHead.MR_JOHNSON, color);
 		this.addDefenseAttributes(3.0, 3.0, 0.10);
-		this.addUnsafeEnchantment (Enchantment.PROTECTION_ENVIRONMENTAL, 2);
+		this.addUnsafeEnchantment (Enchantment.PROTECTION_ENVIRONMENTAL, 3);
 		this.addUnsafeEnchantment(Enchantment.THORNS, 2);
 		this.setDisplayName("Mr. Johnson's Head");
 		this.addLoreToItemMeta("Mr. Johnson wants to clap some pledges tonight.");
