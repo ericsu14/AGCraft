626f9d21b96feb6335704a70bb2fc0670aaadadc
Eric Su <ericsu14@hotmail.com>
2021-03-15 22:09:51 -0700

    Updated Mr. Johnson's head texture to be brighter and changed his feet to be black leather boots instead of netherite

--- AGCraftPlugins/src/com/joojet/plugins/mobs/enums/PlayerHead.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/enums/PlayerHead.java
@@ -23,7 +23,7 @@ public enum PlayerHead {
 	ETERNAL_SHADOW_CLONE_JOOJETSU ("243fadf439f09f3e10d95ee0330eb6a461a6cc3c435f0b2da04af50f1adeda6c"),
 	EVIL_WITHER_SKELETON ("16ca145ba435b375f763ff53b4ce04b2a0c873e8ff547e8b14b392fde6fbfd94"),
 	MR_JOHNSON ("9a08d9aaad22dcb45476fe4ec3ed76ea58027aa9ae5b32400dbac73c5f9d3f10"),
-	MR_JOHNSON_2 ("4947d5a85cf87873b1d115ae7af45739c9f85b50ac213a0db5d94f6695136bbc");
+	MR_JOHNSON_2 ("d5fc73ee7d837b01dc9bb2735b736c09ba0bdd172b690bf1d01cfd6a8df62547");
 	
 	// URL code of playerhead stored in minecraft's skin server
 	private String url;
--- AGCraftPlugins/src/com/joojet/plugins/mobs/equipment/boots/MrJohnsonFeet.java
+++ AGCraftPlugins/src/com/joojet/plugins/mobs/equipment/boots/MrJohnsonFeet.java
@@ -1,22 +1,25 @@
 package com.joojet.plugins.mobs.equipment.boots;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.inventory.EquipmentSlot;
 
 import com.joojet.plugins.agcraft.enums.TextPattern;
 import com.joojet.plugins.agcraft.util.StringUtil;
 import com.joojet.plugins.mobs.enums.EquipmentType;
-import com.joojet.plugins.mobs.equipment.Equipment;
+import com.joojet.plugins.mobs.equipment.LeatherEquipment;
 
-public class MrJohnsonFeet extends Equipment 
+public class MrJohnsonFeet extends LeatherEquipment 
 {
 
 	public MrJohnsonFeet(ChatColor chatColor) 
 	{
-		super(EquipmentType.MR_JOHNSON_FEET, Material.NETHERITE_BOOTS, EquipmentSlot.FEET, chatColor);
+		super(EquipmentType.MR_JOHNSON_FEET, Material.LEATHER_BOOTS, EquipmentSlot.FEET, chatColor);
 		this.setDisplayName(StringUtil.alternateTextColors("Mr. Johnson's Slithery Backend", TextPattern.WORD, 
 				ChatColor.GOLD, ChatColor.GOLD, ChatColor.DARK_GRAY, ChatColor.DARK_BLUE));
+		this.setColor(Color.BLACK);
+		this.addDefenseAttributes(3.0, 2.0, 0.1);
 		this.addSpeedAttribute(0.10);
 		this.makeUnbreakable();
 	}
