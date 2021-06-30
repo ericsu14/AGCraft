3c1d169b2ef0c5396632fbec4ce574e528b34615
Eric Su <ericsu@alumni.usc.edu>
2021-06-30 00:19:25 -0700

    Resolved issue where teleporting a player's nearby tamed entities is unreliable

--- AGCraftPlugins/src/com/joojet/plugins/warp/commands/tasks/AsyncWarpPlayerTask.java
+++ AGCraftPlugins/src/com/joojet/plugins/warp/commands/tasks/AsyncWarpPlayerTask.java
@@ -36,6 +36,48 @@ public class AsyncWarpPlayerTask extends AsyncTask<DatabaseResponse <Location>>
 		this.locationName = locationName;
 	}
 	
+	/** Scans and teleports a player's owned entites to a location.
+	 *  @param player Player being teleported
+	 *  @param location Location in which player is being teleported to
+	 *  @param ownedEntities A list of entities a player is nearby that is able to teleport with the player */
+	public void teleportOwnedEntities (Player player, Location location, List <Entity> ownedEntities)
+	{
+		player.playSound(location, Sound.ENTITY_ENDERMAN_TELEPORT, 0.4f, 1f);
+			
+		// Teleports any player-owned entities to the player's current location as well
+		StringBuilder teleportedEntities = new StringBuilder ();
+		int index = 0;
+		for (Entity entity : ownedEntities)
+		{
+			entity.teleport(player.getLocation());
+				
+			// Appends an "and" to the last element of the string if there is more than one owned entity to teleport
+			if (index == ownedEntities.size() - 1 && ownedEntities.size() > 1)
+			{
+				teleportedEntities = new StringBuilder (teleportedEntities.substring(0, teleportedEntities.length() - 2));
+				teleportedEntities.append(ChatColor.GOLD);
+				teleportedEntities.append(" and ");
+			}
+				
+			teleportedEntities.append(ChatColor.AQUA);
+			teleportedEntities.append(entity.getName());
+				
+			// Appends a comma on the end of the string if there is more than one entity to teleport
+			if (index < ownedEntities.size() - 1)
+			{
+				teleportedEntities.append(", ");
+			}
+				
+			++index;
+		}
+		// Notifies the player that their owned entities are teleported with them.
+		if (!ownedEntities.isEmpty())
+		{
+			player.sendMessage(ChatColor.GRAY + "Teleported " + teleportedEntities.toString() + ChatColor.GRAY + " to your location. Please rejoin the server if they are invisible.");
+		}
+		player.sendMessage(ChatColor.GOLD + "Teleported you to location " + ChatColor.AQUA + locationName + ChatColor.GOLD + " at " + GetCoordinates.getCoordinates(player));
+	}
+	
 	@Override
 	protected DatabaseResponse <Location> getAsyncData() throws SQLException, RuntimeException
 	{
@@ -94,44 +136,11 @@ public class AsyncWarpPlayerTask extends AsyncTask<DatabaseResponse <Location>>
 			return;
 		}
 		
+		List <Entity> ownedEntities = ScanEntities.ScanNearbyPlayerOwnedEntities(player, 40);
+		
 		this.player.teleportAsync(location).thenRun( () -> 
 		{
-			List <Entity> ownedEntities = ScanEntities.ScanNearbyPlayerOwnedEntities(player, 40);
-				
-			player.playSound(location, Sound.ENTITY_ENDERMAN_TELEPORT, 0.4f, 1f);
-				
-			// Teleports any player-owned entities to the player's current location as well
-			StringBuilder teleportedEntities = new StringBuilder ();
-			int index = 0;
-			for (Entity entity : ownedEntities)
-			{
-				entity.teleport(player.getLocation());
-					
-				// Appends an "and" to the last element of the string if there is more than one owned entity to teleport
-				if (index == ownedEntities.size() - 1 && ownedEntities.size() > 1)
-				{
-					teleportedEntities = new StringBuilder (teleportedEntities.substring(0, teleportedEntities.length() - 2));
-					teleportedEntities.append(ChatColor.GOLD);
-					teleportedEntities.append(" and ");
-				}
-					
-				teleportedEntities.append(ChatColor.AQUA);
-				teleportedEntities.append(entity.getName());
-					
-				// Appends a comma on the end of the string if there is more than one entity to teleport
-				if (index < ownedEntities.size() - 1)
-				{
-					teleportedEntities.append(", ");
-				}
-					
-				++index;
-			}
-			// Notifies the player that their owned entities are teleported with them.
-			if (!ownedEntities.isEmpty())
-			{
-				player.sendMessage(ChatColor.GOLD + "Teleported " + teleportedEntities.toString() + ChatColor.GOLD + " to your location. Please rejoin the server if they are invisible.");
-			}
-			player.sendMessage(ChatColor.GOLD + "Teleported you to location " + ChatColor.AQUA + locationName + ChatColor.GOLD + " at " + GetCoordinates.getCoordinates(player));
+			teleportOwnedEntities (player, location, ownedEntities);
 		});
 	}
 
